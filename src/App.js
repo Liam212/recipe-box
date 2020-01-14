66d4dac9fc1eb152c05b6a7b947bca3919a2167a
Liam Stout <Liamstout212@gmail.com>
2020-01-14 21:55:41 +0000

    added nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Recipe from './recipe'
 //import style from './app.module.css';
-import { Container, Row, Button, FormControl, InputGroup} from 'react-bootstrap'
+import { Container, Row, Button, FormControl, InputGroup, Col, Form} from 'react-bootstrap'
 //import InputRange from 'react-input-range'; 
 //import 'react-input-range/lib/css/index.css'
+import Navc from './Navbar'
 const shortid = require('shortid');
+
 require('dotenv').config()
 
 const App = () => {
@@ -40,41 +42,44 @@ const App = () => {
   } 
 
   return (
-    <div>
+    <>
+    <Navc />
     <Container>
-
-      <div>
-        <form onSubmit={getSearch}>
-          <InputGroup className="col-6" style={{marginTop: 15+'px'}}>
-            <FormControl
-              placeholder="Enter an ingredient on your mind..."
-              aria-describedby="basic-addon2"
-              type="text" value={search} onChange={updateSearch}
-            />
-            <InputGroup.Append>
-              <Button variant="outline-secondary" type="submit">Search</Button>
-            </InputGroup.Append>
-          </InputGroup>
-        </form>
-      </div>
-      <Row className="justify-content-center">
-        {recipes.length === 0 ? (
-          <div>
-            <h3 style={{marginTop: 15+'px'}}>No recipes found please try again</h3>
-          </div>
-        ) : (
-        recipes.map(recipe => (
-          <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories}
-            source={recipe.recipe.source} 
-            url={recipe.recipe.url}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
-            key={shortid.generate()}/>
-        ))
-        )}
+      <Row>
+      <Col lg={12}>
+        <Form className="col-7 d-flex" onSubmit={getSearch}>
+            <InputGroup style={{marginTop: 15+'px'}}>
+              <FormControl
+                placeholder="Enter an ingredient on your mind..."
+                aria-describedby="basic-addon2"
+                type="text" value={search} onChange={updateSearch}
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" type="submit">Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </Col>
+     
         </Row>
+        <Row className="justify-content-center">
+          {recipes.length === 0 ? (
+            <div>
+              <h3 style={{marginTop: 15+'px', fontWeight: 200}}>No recipes found please try again</h3>
+            </div>
+          ) : (
+          recipes.map(recipe => (
+            <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories}
+              source={recipe.recipe.source} 
+              url={recipe.recipe.url}
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+              key={shortid.generate()}/>
+          ))
+          )}
+          </Row>
       </Container>
-    </div>
+      </>
   )
 
 }
